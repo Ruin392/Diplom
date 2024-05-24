55ae0913b7bfb00ac4adf89efce0abb5f8d7bc87
Ayupov_laptop <ruin392@gmail.com>
2024-05-24 17:47:43 +0300

    add network + meta + main

--- main.tf
+++ main.tf
@@ -1,54 +1,253 @@
-variable "yandex_cloud_token" {
-  type        = string
-  description = "Enter secret token"
+#variable "yandex_cloud_token" {
+#  type        = string
+#  description = "Enter secret token"
+#}
+
+variable "image_id" {
+  type = string
+  description = "fd87kbts7j40q5b9rpjr"
 }
 
+
 provider "yandex" {
-  token     = var.yandex_cloud_token
+  #token     = var.yandex_cloud_token
+  token     = ""
   cloud_id  = "b1ghqvpq8ojemmvlnup8"
   folder_id = "b1glausc3p7sms17sseu"
   #  zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
-  zone = "ru-central1-a"
+#----------------- WWW -----------------------------
+resource "yandex_compute_instance" "nginx-1" {
+  name        = "vm-nginx-1"
+  hostname    = "nginx-1"
+  zone        = "ru-central1-a"
 
   resources {
     cores  = 2
     memory = 2
+    core_fraction = 20
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd87kbts7j40q5b9rpjr"
+      image_id = var.image_id
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    subnet_id          = yandex_vpc_subnet.inner-nginx-1.id
+    security_group_ids = [yandex_vpc_security_group.inner.id]
+    ip_address         = "10.0.1.3"
   }
 
   metadata = {
     user-data = "${file("./meta.txt")}"
   }
 
-}
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  scheduling_policy {
+    preemptible = true
+  }
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+resource "yandex_compute_instance" "nginx-2" {
+  name        = "vm-nginx-2"
+  hostname    = "nginx-2"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.inner-nginx-2.id
+    security_group_ids = [yandex_vpc_security_group.inner.id]
+    ip_address         = "10.0.2.3"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
 }
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+
+#----------------- bastion -----------------------------
+resource "yandex_compute_instance" "bastion" {
+  name        = "vm-bastion"
+  hostname    = "bastion"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd82v0f4ufbnvm3b9s08" # NAT-�������
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.inner.id, yandex_vpc_security_group.public-bastion.id]
+    ip_address         = "10.0.4.4"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+
+##----------------- prometheus -----------------------------
+resource "yandex_compute_instance" "prometheus" {
+  name        = "vm-prometheus"
+  hostname    = "prometheus"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.inner-services.id
+    security_group_ids = [yandex_vpc_security_group.inner.id]
+    ip_address         = "10.0.3.3"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+#----------------- grafana -----------------------------
+resource "yandex_compute_instance" "grafana" {
+  name        = "vm-grafana"
+  hostname    = "grafana"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.inner.id, yandex_vpc_security_group.public-grafana.id]
+    ip_address         = "10.0.4.5"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+#----------------- elastic -----------------------------
+resource "yandex_compute_instance" "elastic" {
+  name        = "vm-elastic"
+  hostname    = "elastic"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 4
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = 15
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.inner-services.id
+    security_group_ids = [yandex_vpc_security_group.inner.id]
+    ip_address         = "10.0.3.4"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+#----------------- kibana -----------------------------
+resource "yandex_compute_instance" "kibana" {
+  name        = "vm-kibana"
+  hostname    = "kibana"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.inner.id, yandex_vpc_security_group.public-kibana.id]
+    ip_address         = "10.0.4.3"
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
 }
